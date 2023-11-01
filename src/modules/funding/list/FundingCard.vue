2164d9181c91804b885546991c51135d9a4fdbbb
binarywoo27 <blu_mountain@naver.com>
2023-11-01 19:52:38 +0900

    feat: 펀딩내역 목록 조회

--- src/modules/funding/list/FundingCard.vue
+++ src/modules/funding/list/FundingCard.vue
@@ -1,41 +1,108 @@
 <template>
-  <div class='funding-list-card-wrapper'>
+  <div v-for='(funding, index) in fundings' :key='index' class='funding-list-card-wrapper'>
     <div class='funding-list-header'>
       <div class='funding-list-left'>
         <div
-            style='border: 1px solid; width: 15px; height: 15px; border-radius: 50px; background-color: #4fafb1'></div>
-        <div class='funding-list-status'>진행중</div>
+          style='border: 1px solid; width: 15px; height: 15px; border-radius: 50px; background-color: #4fafb1'></div>
+        <div class='funding-list-status'>{{ funding.status }}</div>
       </div>
+      <div style='width: 20%;'></div>
       <div class='funding-list-right'>
-        2023.09.25 참여
+        {{ funding.participationDate }} 참여
       </div>
     </div>
     <div class='funding-list-body'>
       <div class='funding-list-img'>
-        <img
-            height='80px;'
-            src='https://cdn.wadiz.kr/wwwwadiz/green001/2023/0901/20230901151029859_236909.jpg/wadiz/resize/1200/format/jpg/quality/80/'
-            width='120px' />
+        <img :src='funding.imageUrl' alt='product img' style='width: 240px;' />
+      </div>
+      <div class='funding-list-contents'>
+        <div class='funding-list-contents-header' style='color: #58C1C2;text-align: center;font-size: 20px;'>
+          {{ funding.achievement }}% 달성
+        </div>
+        <div class='funding-list-contents-item'>
+          {{ funding.title }}
+        </div>
+        <div class='funding-list-contents-item'>
+          <div class='funding-inner-contents'>
+            {{ funding.amountAchieved }}
+            <div style='font-size: 10px'> 원 달성</div>
+          </div>
+
+          <div class='funding-inner-contents'>
+            참여자 수: {{ funding.participantCount }} 명
+          </div>
+          <div class='funding-inner-contents'>
+            {{ funding.daysLeft }}일 남음
+          </div>
+        </div>
+        <div class='funding-list-contents-item'>
+          {{ funding.provider }}
+        </div>
+        <div class='funding-list-contents-item'>
+          내 결제 금액: {{ funding.paymentAmount }}
+        </div>
+      </div>
+    </div>
+    <div class='funding-list-bottom'>
+      <div class='funding-status'>
+        펀딩/배송상태: {{ funding.fundingStatus }}
+      </div>
+      <div style='width: 20%;'></div>
+      <div class='funding-details-btn'>
+        상세보기 >
       </div>
     </div>
   </div>
 </template>
 
 <script lang='ts' setup>
+import { ref } from 'vue'
+
+const fundings = ref([
+  {
+    status: '진행중',
+    participationDate: '2023.09.25',
+    imageUrl: 'https://cdn.wadiz.kr/wwwwadiz/green001/2023/0901/20230901151029859_236909.jpg/wadiz/resize/1200/format/jpg/quality/80/',
+    achievement: '3478% 달성',
+    title: '평점5.0/마지막수확ㅣ진~하게 완숙된 초 달큰한 무화과, 홍&청무화과세트',
+    amountAchieved: '28,100,000원',
+    participantCount: '250명',
+    daysLeft: '7일 남음',
+    provider: 'by 올데이온(alldayON)',
+    paymentAmount: '78,900원',
+    fundingStatus: '결제 취소'
+  },
+  {
+    status: '진행중',
+    participationDate: '2023.09.25',
+    imageUrl: 'https://cdn.wadiz.kr/wwwwadiz/green001/2023/0901/20230901151029859_236909.jpg/wadiz/resize/1200/format/jpg/quality/80/',
+    achievement: '3478% 달성',
+    title: '평점5.0/마지막수확ㅣ진~하게 완숙된 초 달큰한 무화과, 홍&청무화과세트',
+    amountAchieved: '28,100,000원',
+    participantCount: '250명',
+    daysLeft: '7일 남음',
+    provider: 'by 올데이온(alldayON)',
+    paymentAmount: '78,900원',
+    fundingStatus: '결제 취소'
+  }
+])
 </script>
 
-<style>
+<style scoped>
 .funding-list-card-wrapper {
   display: inline-flex;
   padding: 15px 0px;
   flex-direction: column;
   align-items: flex-start;
   padding: 10px;
+  border: 1px solid gray;
+  width: 1410px;
+  margin: 20px 0;
 }
 
 .funding-list-header {
   display: flex;
-  width: 1600px;
+  width: 1400px;
   padding: 0px 30px;
   align-items: center;
 }
@@ -57,7 +124,7 @@
 
 .funding-list-right {
   display: flex;
-  width: 70%;
+  width: 60%;
   padding: 10px 0px;
   justify-content: flex-end;
   align-items: center;
@@ -68,17 +135,68 @@
 .funding-list-body {
   display: flex;
   align-items: center;
-  gap: 7px;
+  gap: 15px;
+  padding: 0 30px;
 }
 
 .funding-list-img {
   display: flex;
-  width: 402px;
-  height: 246px;
   padding: 10px;
+}
+
+.funding-list-contents {
+  display: flex;
   flex-direction: column;
-  justify-content: center;
+  align-items: flex-start;
+}
+
+.funding-list-contents-header {
+  display: flex;
+  width: 1191px;
+  padding: 10px;
   align-items: center;
   gap: 10px;
+}
+
+.funding-list-contents-item {
+  display: flex;
+  width: 1191px;
+  padding: 0px 10px;
+  align-items: center;
+  gap: 85px;
+}
+
+.funding-inner-contents {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+.funding-list-bottom {
+  display: flex;
+  width: 1575px;
+  align-items: flex-start;
+//gap: 1118px;
+}
+
+.funding-status {
+  width: 20%;
+  display: flex;
+  padding: 10px;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 10px;
+  flex-shrink: 0;
+  padding: 0 40px;
+}
+
+.funding-details-btn {
+  display: flex;
+  width: 40%;
+  padding: 10px;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 10px;
+  flex-shrink: 0;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -24,216 +24,358 @@ import ProjectImageSectionComponent from '@/modules/project/components/ProjectIm
 import FundingListView from '@/views/FundingListView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'main',
-      component: MainView
-    },
-    {
-      path: '/member/sign-in',
-      name: 'member-sign-in',
-      component: () => import('../views/member/SignIn.vue')
-    },
-    {
-      path: '/member/sign-up',
-      component: () => import('../views/member/SignUp.vue'),
-      children: [
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
         {
-          path: '',
-          name: 'member-sign-up-agreement',
-          component: () => import('../modules/member/components/SignUpAgreement.vue')
+            path: '/',
+            name: 'main',
+            component: MainView
         },
         {
-          path: 'basic-info',
-          name: 'member-sign-up-basic-info',
-          component: () => import('../modules/member/components/SignUpBasicInfo.vue')
+            path: '/member/sign-in',
+            name: 'member-sign-in',
+            component: () => import('../views/member/SignIn.vue')
         },
         {
-          path: 'email-auth',
-          name: 'member-sign-up-email-auth',
-          component: () => import('../modules/member/components/SignUpEmailAuth.vue')
+            path: '/member/sign-up',
+            component: () => import('../views/member/SignUp.vue'),
+            children: [
+                {
+                    path: '',
+                    name: 'member-sign-up-agreement',
+                    component: () => import('../modules/member/components/SignUpAgreement.vue')
+                },
+                {
+                    path: 'basic-info',
+                    name: 'member-sign-up-basic-info',
+                    component: () => import('../modules/member/components/SignUpBasicInfo.vue')
+                },
+                {
+                    path: 'email-auth',
+                    name: 'member-sign-up-email-auth',
+                    component: () => import('../modules/member/components/SignUpEmailAuth.vue')
+                },
+                {
+                    path: 'success',
+                    name: 'member-sign-up-success',
+                    component: () => import('../modules/member/components/SignUpSuccess.vue')
+                }
+            ]
         },
         {
-          path: 'success',
-          name: 'member-sign-up-success',
-          component: () => import('../modules/member/components/SignUpSuccess.vue')
+            path: '/member/my-page',
+            component: () => import('../views/member/MyPage.vue'),
+            children: [
+                {
+                    path: '',
+                    name: 'my-page-supporter',
+                    component: () => import('../modules/member/components/MyPageSupporter.vue'),
+                    meta: { authRequired: true }
+                },
+                {
+                    path: 'maker',
+                    name: 'my-page-maker',
+                    component: () => import('../modules/member/components/MyPageMaker.vue'),
+                    meta: { authRequired: true }
+                },
+                {
+                    path: 'change',
+                    name: 'my-page-info-change',
+                    component: () => import('../modules/member/components/MemberInfoChange.vue'),
+                    meta: { authRequired: true }
+                }
+            ],
+            meta: { authRequired: true }
+        },
+        {
+            path: '/member/membership-honors',
+            name: 'membership-honors',
+            component: () => import('../views/member/MembershipPayments.vue'),
+            meta: { authRequired: true }
+        },
+        {
+            path: '/member/membership-honors/join/success',
+            name: 'membership-honors-join',
+            component: () => import('../modules/member/components/MembershipPaymentsCompleted.vue'),
+            meta: { authRequired: true }
+        },
+        {
+            path: '/member/likes-list',
+            name: 'member-likes-list',
+            component: () => import('../views/member/LikesList.vue'),
+            meta: { authRequired: true }
+        },
+        {
+            path: '/notifications',
+            name: 'notifications',
+            component: NotificationView,
+            meta: { authRequired: true }
+        },
+        {
+            path: '/projects/category',
+            name: 'projectListByCategory',
+            component: ProjectListByCategoryView
+        },
+        {
+            path: '/projects/details',
+            name: 'projectDetails',
+            component: ProjectDetailsView
+        },
+        {
+            path: '/lotdeal',
+            name: 'lotdeal',
+            component: LotdealView
+        },
+        {
+            path: '/special-exhibition/camping',
+            name: 'specialExhibition',
+            component: SpecialExhibitionView
+        },
+        {
+            path: '/project-details/:id',
+            name: 'projectDetails',
+            component: ProjectDetailsView,
+            children: [
+                {
+                    path: '',
+                    name: 'projectStory',
+                    component: ProjectImageSectionComponent
+                },
+                {
+                    path: 'support-signature',
+                    component: SupportSignatureComponent
+                },
+                {
+                    path: 'supporter-with-us',
+                    component: SupporterWithUsComponent
+                }
+            ]
+        },
+        {
+            path: '/funding',
+            name: 'funding',
+            component: FundingView
+        },
+        {
+            path: '/payments/approve/:order/:user',
+            name: 'payments',
+            component: FundingPayApproveView
+        },
+        {
+            path: '/funding/details',
+            name: 'fundingDetails',
+            component: FundingDetailsView
+        },
+        {
+            path: '/funding/list',
+            name: 'fundingList',
+            component: FundingListView
+        },
+        {
+            path: '/projects',
+            name: 'projectRegister',
+            component: ProjectRegisterView,
+            children: [
+                {
+                    path: 'basic-information',
+                    component: DefaultInformation
+                },
+                {
+                    path: 'maker',
+                    component: MakerRegister
+                },
+                {
+                    path: 'project-info',
+                    component: ProjectInformation
+                },
+                {
+                    path: 'story',
+                    component: WriteStory
+                },
+                {
+                    path: 'products',
+                    component: RegisterProducts
+                }
+            ],
+            meta: { authRequired: true }
+        },
+        {
+            path: '/maker/projects',
+            component: RegisteredProjects,
+            meta: { authRequired: true }
+        },
+        {
+            path: '/member/my-page',
+            component: () => import('../views/member/MyPage.vue'),
+            children: [
+                {
+                    path: '',
+                    name: 'my-page-supporter',
+                    component: () => import('../modules/member/components/MyPageSupporter.vue'),
+                    meta: { authRequired: true }
+                },
+                {
+                    path: 'maker',
+                    name: 'my-page-maker',
+                    component: () => import('../modules/member/components/MyPageMaker.vue'),
+                    meta: { authRequired: true }
+                },
+                {
+                    path: 'change',
+                    name: 'my-page-info-change',
+                    component: () => import('../modules/member/components/MemberInfoChange.vue'),
+                    meta: { authRequired: true }
+                }
+            ],
+            meta: { authRequired: true }
+        },
+        {
+            path: '/member/membership-honors',
+            name: 'membership-honors',
+            component: () => import('../views/member/MembershipPayments.vue'),
+            meta: { authRequired: true }
+        },
+        {
+            path: '/member/membership-honors/join/success',
+            name: 'membership-honors-join',
+            component: () => import('../modules/member/components/MembershipPaymentsCompleted.vue'),
+            meta: { authRequired: true }
+        },
+        {
+            path: '/member/likes-list',
+            name: 'member-likes-list',
+            component: () => import('../views/member/LikesList.vue'),
+            meta: { authRequired: true }
+        },
+        {
+            path: '/notifications',
+            name: 'notifications',
+            component: NotificationView,
+            meta: { authRequired: true }
+        },
+        {
+            path: '/projects/category',
+            name: 'projectListByCategory',
+            component: ProjectListByCategoryView
+        },
+        {
+            path: '/projects/details',
+            name: 'projectDetails',
+            component: ProjectDetailsView
+        },
+        {
+            path: '/lotdeal',
+            name: 'lotdeal',
+            component: LotdealView
+        },
+        {
+            path: '/special-exhibition/camping',
+            name: 'specialExhibition',
+            component: SpecialExhibitionView
+        },
+        {
+            path: '/project-details/:id',
+            name: 'projectDetails',
+            component: ProjectDetailsView,
+            children: [
+                {
+                    path: '',
+                    name: 'projectStory',
+                    component: ProjectImageSectionComponent
+                },
+                {
+                    path: 'support-signature',
+                    component: SupportSignatureComponent
+                },
+                {
+                    path: 'supporter-with-us',
+                    component: SupporterWithUsComponent
+                }
+            ]
+        },
+        {
+            path: '/funding',
+            name: 'funding',
+            component: FundingView,
+            meta: { authRequired: true }
+        },
+        {
+            path: '/payments/approve/:order/:user',
+            name: 'payments',
+            component: FundingPayApproveView,
+            meta: { authRequired: true }
+        },
+        {
+            path: '/funding/details',
+            name: 'fundingDetails',
+            component: FundingDetailsView,
+            meta: { authRequired: true }
+        },
+        {
+            path: '/funding/list',
+            name: 'fundingList',
+            component: FundingListView,
+            meta: { authRequired: true }
+        },
+        {
+            path: '/projects',
+            name: 'projectRegister',
+            component: ProjectRegisterView,
+            children: [
+                {
+                    path: 'basic-information',
+                    component: DefaultInformation
+                },
+                {
+                    path: 'maker',
+                    component: MakerRegister
+                },
+                {
+                    path: 'project-info',
+                    component: ProjectInformation
+                },
+                {
+                    path: 'story',
+                    component: WriteStory
+                },
+                {
+                    path: 'products',
+                    component: RegisterProducts
+                }
+            ],
+            meta: { authRequired: true }
+        },
+        {
+            path: '/maker/projects',
+            component: RegisteredProjects,
+            meta: { authRequired: true }
         }
-      ]
-    },
-    {
-      path: '/member/my-page',
-      component: () => import('../views/member/MyPage.vue'),
-      children: [
-        {
-          path: '',
-          name: 'my-page-supporter',
-          component: () => import('../modules/member/components/MyPageSupporter.vue'),
-          meta: { authRequired: true }
-        },
-        {
-          path: 'maker',
-          name: 'my-page-maker',
-          component: () => import('../modules/member/components/MyPageMaker.vue'),
-          meta: { authRequired: true }
-        },
-        {
-          path: 'change',
-          name: 'my-page-info-change',
-          component: () => import('../modules/member/components/MemberInfoChange.vue'),
-          meta: { authRequired: true }
-        }
-      ],
-      meta: { authRequired: true }
-    },
-    {
-      path: '/member/membership-honors',
-      name: 'membership-honors',
-      component: () => import('../views/member/MembershipPayments.vue'),
-      meta: { authRequired: true }
-    },
-    {
-      path: '/member/membership-honors/join/success',
-      name: 'membership-honors-join',
-      component: () => import('../modules/member/components/MembershipPaymentsCompleted.vue'),
-      meta: { authRequired: true }
-    },
-    {
-      path: '/member/likes-list',
-      name: 'member-likes-list',
-      component: () => import('../views/member/LikesList.vue'),
-      meta: { authRequired: true }
-    },
-    {
-      path: '/notifications',
-      name: 'notifications',
-      component: NotificationView,
-      meta: { authRequired: true }
-    },
-    {
-      path: '/projects/category',
-      name: 'projectListByCategory',
-      component: ProjectListByCategoryView
-    },
-    {
-      path: '/projects/details',
-      name: 'projectDetails',
-      component: ProjectDetailsView
-    },
-    {
-      path: '/lotdeal',
-      name: 'lotdeal',
-      component: LotdealView
-    },
-    {
-      path: '/special-exhibition/camping',
-      name: 'specialExhibition',
-      component: SpecialExhibitionView
-    },
-    {
-      path: '/project-details/:id',
-      name: 'projectDetails',
-      component: ProjectDetailsView,
-      children: [
-        {
-          path: '',
-          name: 'projectStory',
-          component: ProjectImageSectionComponent
-        },
-        {
-          path: 'support-signature',
-          component: SupportSignatureComponent
-        },
-        {
-          path: 'supporter-with-us',
-          component: SupporterWithUsComponent
-        }
-      ]
-    },
-    {
-      path: '/funding',
-      name: 'funding',
-      component: FundingView,
-      meta: { authRequired: true }
-    },
-    {
-      path: '/payments/approve/:order/:user',
-      name: 'payments',
-      component: FundingPayApproveView,
-      meta: { authRequired: true }
-    },
-    {
-      path: '/funding/details',
-      name: 'fundingDetails',
-      component: FundingDetailsView,
-      meta: { authRequired: true }
-    },
-    {
-      path: '/funding/list',
-      name: 'fundingList',
-      component: FundingListView,
-      meta: { authRequired: true }
-    },
-    {
-      path: '/projects',
-      name: 'projectRegister',
-      component: ProjectRegisterView,
-      children: [
-        {
-          path: 'basic-information',
-          component: DefaultInformation
-        },
-        {
-          path: 'maker',
-          component: MakerRegister
-        },
-        {
-          path: 'project-info',
-          component: ProjectInformation
-        },
-        {
-          path: 'story',
-          component: WriteStory
-        },
-        {
-          path: 'products',
-          component: RegisterProducts
-        }
-      ],
-      meta: { authRequired: true }
-    },
-    {
-      path: '/maker/projects',
-      component: RegisteredProjects,
-      meta: { authRequired: true }
-    }
-  ]
+    ]
 })
 
 router.beforeEach(async (to, from, next) => {
 
-  const jwtToken = localStorage.getItem('jwtToken')
+    const jwtToken = localStorage.getItem('jwtToken')
 
-  if (jwtToken !== null) {
-    client.interceptors.request.use((config) => {
-      config.headers.setAuthorization(jwtToken)
-      return config
-    }, (error) => Promise.reject(error))
-  }
-
-  if (to.matched.some(record => record.meta.authRequired)) {
-    if (jwtToken === null) {
-      alert('로그인이 필요한 페이지 입니다.')
-      next({
-        path: '/member/sign-in',
-        query: { redirect: to.fullPath }
-      })
-
-      alert('로그인이 필요한 페이지 입니다.')
+    if (jwtToken !== null) {
+        client.interceptors.request.use((config) => {
+            config.headers.setAuthorization(jwtToken)
+            return config
+        }, (error) => Promise.reject(error))
     }
-  }
-  next()
+
+    if (to.matched.some(record => record.meta.authRequired)) {
+        if (jwtToken === null) {
+            alert('로그인이 필요한 페이지 입니다.')
+            next({
+                path: '/member/sign-in',
+                query: { redirect: to.fullPath }
+            })
+
+            alert('로그인이 필요한 페이지 입니다.')
+        }
+    }
+    next()
 })
 
 export default router
